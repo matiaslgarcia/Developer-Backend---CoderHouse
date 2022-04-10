d6ee5c153da306b1d7ab1a00c25f31c098ae7384
Matias Garcia <matias.garcia@fierastudio.com>
2022-04-10 13:55:10 -0300

    desafio1

---
+++ desafio1.js
@@ -0,0 +1,49 @@
+class Usuario {
+    constructor(nombre, apellido, libros, mascotas) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas
+    }
+
+    getFullName = () =>{
+        return `El Usuario se llama ${this.nombre} ${this.apellido}`
+    }
+
+    addMascota = (nombreMascota) => {
+        this.mascotas.push(nombreMascota)
+    }
+
+    countMascotas = () =>{
+        return this.mascotas.length
+    }
+
+    addBook = (nombreLibro, autorLibro) => {
+        this.libros.push({nombre: nombreLibro, autor: autorLibro})
+    }
+
+    getBookNames = () => {
+        return this.libros.map(nameBook => nameBook.nombre)
+    }
+}
+
+const libross = [
+    {
+        nombre: 'Vivir el camino',
+        autor: 'Palermo',
+    },
+    {
+        nombre:'Pasaje al Futuro',
+        autor: 'Bilinkis',
+    }
+    ]
+const unUsu = new Usuario('Matias','Garcia', libross, ['uhma', 'oli','peni'])
+
+console.log(unUsu.getFullName())
+unUsu.addMascota('pepito')
+console.log('La cantidad de mascotas que tiene el usuario es: ' + unUsu.countMascotas())
+
+unUsu.addBook('Inteligencia Emocional', 'Goleman')
+console.log(unUsu.libros)
+
+console.log('Los nombres de los libros del usuario son: ' + '\t' + unUsu.getBookNames())
