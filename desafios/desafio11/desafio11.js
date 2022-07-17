fac1bf85943cf3ffdeee9023ab4a65390a9d9a64
Matias Garcia <matias.garcia@fierastudio.com>
2022-07-17 16:58:45 -0300

    refactor

--- desafios/desafio11/desafio11.js
+++ desafios/desafio11/desafio11.js
@@ -2,7 +2,6 @@ import express from 'express'
 import mongoose from 'mongoose'
 import session from 'express-session';
 import MongoStore from 'connect-mongo';
-import path from 'path';
 import { createServer } from "http";
 import { Server } from "socket.io";
 import knex from 'knex'
@@ -16,7 +15,6 @@ import passport from 'passport'
 import { Strategy as LocalStrategy} from 'passport-local'
 faker.locale = 'es'
 
-const __dirname = path.resolve();
 const optionsMariaDB = {
   client: 'mysql',
   connection: {
@@ -30,10 +28,51 @@ const knex1 = knex(optionsMariaDB)
 
 const URL = "mongodb+srv://coderhouse:coderhouse@cluster0.utluy.mongodb.net/?retryWrites=true&w=majority"
 let conexion = mongoose.connect(URL);
+const advancedOptions = {useNewUrlParser: true, useUnifiedTopology: true}
+//Instancias
+const dbProduct = new MetodosDB(knex1, 'productos')
+await dbProduct.crearTablaProductos()
+const contenedor = new Contenedor(knex1, 'productos')
+
+const mensajes = new ContenedorMongoMensajes(conexion)
+const producto = new Producto()
+const user = new ContenedorMongoUsuarios(conexion)
+
+//MIDDLEWARE
+passport.use('register', new LocalStrategy({
+    passReqToCallback: true
+  },async (req, password, done) => {
+    const { email } = req.body
+    const usuario = await user.findUser(email)
+    console.log(usuario)
+    if(usuario){
+      return done('Usuario registrado')
+    }
+    const userNuevo = {
+      email: email,
+      password: password,
+    }
+    await user.saveUsuario(userNuevo)
+    return done(null, userNuevo)
+  }
+))
+
+passport.use('login', new LocalStrategy({
+  passReqToCallback: true
+}, async (req, password,done) => {
+  const {email} = req.body;
+  const usuario = await user.findUser(email)
+  console.log(usuario)
+  if(!usuario){
+    return done(null,false)
+  }
+  if(usuario.password !== password){
+    return done(null,false)
+  }
+  return done(null,usuario)
+}))
 
 const app = express()
-
-const advancedOptions = {useNewUrlParser: true, useUnifiedTopology: true}
 
 app.use(
   session({
@@ -49,100 +88,47 @@ app.use(
     }
   })  
 )
-const httpServer = createServer(app)
-const io = new Server(httpServer)
 
-const producto = new Producto()
-
-app.set('view engine', 'ejs')
- 
-//MIDDLEWARE
-app.use(express.static('./public'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-
-//Instancias
-const dbProduct = new MetodosDB(knex1, 'productos')
-dbProduct.crearTablaProductos()
-  .then(r => r)
-  .catch(e => console.log('Error:' + e))
-
-const contenedor = new Contenedor(knex1, 'productos')
-
-const mensajes = new ContenedorMongoMensajes(conexion)
-
-const user = new ContenedorMongoUsuarios(conexion)
-
-passport.use('register', new LocalStrategy({
-  passReqToCallback: true
-  },async (req, password, done) => {
-      const { email } = req.body
-      const usuario = await user.findUser(email)
-      console.log(usuario)
-      if(usuario){
-          return done('Usuario registrado')
-      }
-      const userNuevo = {
-          username: email,
-          password: password,
-      }
-      await user.saveUsuario(userNuevo)
-      return done(null, userNuevo)
-  }
-))
-
-passport.use('login', new LocalStrategy(
-  async (req, password,done) => {
-    const {email} = req.body;
-    const usuario = await user.findUser(email)
-      if(!usuario){
-          return done(null,false)
-      }
-      if(usuario.password != password){
-          return done(null,false)
-      }
-    return done(null,usuario)
-}))
-
-passport.serializeUser(function (req,done){
-  const {email} = req.email
-  done(null,email)
+passport.serializeUser(function (usuario,done){
+  done(null,usuario.email)
 })
 
-passport.deserializeUser( async function (req, done){
-  const {email} = req.email
+passport.deserializeUser( async function (email, done){
   const usuario = await user.findUser(email)
   done(null, usuario)
 })
 
+const httpServer = createServer(app)
+const io = new Server(httpServer)
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.static('./public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+
 function autorizacionWeb(req, res, next) {
-  if (req.session?.nombre) {
+  if(req.isAuthenticated()){
       next()
   } else {
       res.redirect('/login')
   }
 }
 
-app.use(passport.initialize())
-app.use(passport.session())
 //EndPoint
 //Para login
 
-app.get('/', (req, res) => {
-  if(req.isAuthenticated()){
+app.get('/', autorizacionWeb, (req, res) => {
     res.redirect('/landing')
-  }else{
-    res.redirect('/login')
-  }
 })
 
 app.get('/logout', (req, res) => {
-  const nombre = req.session?.nombre
-  if (nombre) {
+  const email = req.session?.email
+  if (email) {
       req.session.destroy(err => {
           if (!err) {
-              res.render('principalDeslogueo.ejs', { nombre })
+              res.render('principalDeslogueo.ejs', { email })
           } else {
               res.redirect('/')
           }
@@ -153,9 +139,8 @@ app.get('/logout', (req, res) => {
 })
 
 app.get('/landing', autorizacionWeb, (req, res) => {
-  res.render('principal.ejs', { nombre: req.session.nombre })
+  res.render('principal.ejs', { email: req.session.email })
 })
-
 
 app.get('/api/productos-test' ,async (req, res) => {
   try{
@@ -172,30 +157,21 @@ app.get('/login', (req, res) => {
   }
   res.render('principalLogueoUsuario.ejs')
 })
-
-app.post('/login', (req, res) => {
-  const { nombre, password } = req.body
-  const existe = user.findUser(nombre)
-  if(existe){
-    return res.redirect('/landing')
-  }
-  const usuario = { 
-    email: nombre,
-    password: password 
-  }
-  user.saveUsuario(usuario)
-  return res.redirect('/landing')
+app.get('/faillogin',(req,res) => {
+  res.render('principalErrorLogin.ejs')
 })
 
-app.post('/register', passport.authenticate('register', {failureRedirect: '/failregister', successRedirect:'/'}))
+app.post('/login',passport.authenticate('login', {failureRedirect: '/faillogin', successRedirect: '/landing'}))
 
 app.get('/register', async (req, res) => {
   res.render('principalRegistrarUsuario.ejs')
 })
 
 app.get('/failregister',(req,res) => {
-  res.render('principalErrorLogin.ejs')
+  res.render('principalErrorSignup.ejs')
 })
+
+app.post('/register', passport.authenticate('register', {failureRedirect: '/failregister', successRedirect:'/'}))
 
 //SOCKET
 setTimeout(() =>{
