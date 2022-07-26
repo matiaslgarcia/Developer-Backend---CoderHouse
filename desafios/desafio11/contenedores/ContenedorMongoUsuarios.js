56a3060927f0f173ba5b1c89dada5bca2f1116de
matiaslgarcia <matiasgarcia444@gmail.com>
2022-07-26 18:56:38 -0300

    ahora almacena contraseña para desafio 11

--- desafios/desafio11/contenedores/ContenedorMongoUsuarios.js
+++ desafios/desafio11/contenedores/ContenedorMongoUsuarios.js
@@ -10,7 +10,7 @@ export default class ContenedorMongoUsuarios{
     try {
         const newUsuario = {
             "email": nuevoUsuario.email,
-            "password": nuevoUsuario.password,
+            "passwordHash": nuevoUsuario.passwordHash,
         }
         await usuarios.usuarios.create(newUsuario)
     }catch(error){
