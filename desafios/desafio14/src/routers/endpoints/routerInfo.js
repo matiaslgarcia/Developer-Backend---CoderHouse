b6252d2104918de2aaace9c8be6c70f91e45cc93
matiaslgarcia <AlexisMasin86@gmail.com>
2022-08-16 18:53:58 -0300

    Desafio 14 concluido

--- desafios/desafio14/src/routers/endpoints/routerInfo.js
+++ desafios/desafio14/src/routers/endpoints/routerInfo.js
@@ -7,8 +7,8 @@ const generarInfo = new Router()
 
 generarInfo.get('/info' ,async (req, res) => {
     try{
-        logger.info(await info.solicitarInformacion())
-        // res.render('principalInfo.ejs',{informacion: await info.solicitarInformacion()})
+        //console.log(await info.solicitarInformacion())
+        res.render('principalInfo.ejs',{informacion: await info.solicitarInformacion()})
     }catch (e) {
       res.send(e)
     }
@@ -23,3 +23,27 @@ generarInfo.get('/info-compression', compression(), async (req, res) => {
 })
 
 export default generarInfo
+
+/* -------------------------- DEBUG ---------------------------- */
+
+//node --prof server.js
+// node --prof-process slow-v8.log > prof_slow.txt
+// node --inspect server.js
+// chrome://inspect
+//
+// AMBOS TEST CON
+// artillery quick -c 50 -n 20 "http://localhost:8080/info" > artillery_slow.txt
+//
+// /* ----------------------------------------------------------- */
+// /* -------------------------- NO_DEBUG ---------------------------- */
+//modo bloqueante
+// node --prof server.js//node --prof-process fast-v8.log > prof_fast.txt
+// node --inspect server.js
+// chrome://inspect
+// AMBOS TEST CON
+// /artillery quick -c 50 -n 20 "http://localhost:8080/info" > artillery_fast.txt
+// /* ------------------------------------------------------
+
+//Autocannon
+// 1ro: prender servidor npm start
+// 2do: en otra consola, curl -X GET "http://localhost:8080/info"
