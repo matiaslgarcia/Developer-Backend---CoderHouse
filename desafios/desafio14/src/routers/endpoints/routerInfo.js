1ebf31ad03eb27a1f7c6556a01b7b052a840b63f
Matias Garcia <matias.garcia@fierastudio.com>
2022-08-16 16:05:40 -0300

    some changes

--- desafios/desafio14/src/routers/endpoints/routerInfo.js
+++ desafios/desafio14/src/routers/endpoints/routerInfo.js
@@ -1,12 +1,14 @@
 import info from '../../api/instanciaInformacion.js'
 import { Router } from "express"
 import compression from 'compression'
+import logger from "../../../utils/logger.js";
 
 const generarInfo = new Router()
 
 generarInfo.get('/info' ,async (req, res) => {
     try{
-        res.render('principalInfo.ejs',{informacion: await info.solicitarInformacion()})
+        logger.info(await info.solicitarInformacion())
+        // res.render('principalInfo.ejs',{informacion: await info.solicitarInformacion()})
     }catch (e) {
       res.send(e)
     }
--- desafios/desafio14/src/routers/endpoints/routerRandom.js
+++ desafios/desafio14/src/routers/endpoints/routerRandom.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
-import { fork } from 'child_process'
+// import { fork } from 'child_process'
 import path from 'path'
 
 const generarRandom = new Router()
 
 generarRandom.get('/randoms/:cantidad?', (req, res) => {
-    const computo = fork(path.resolve(process.cwd(),'../../../utils/random.js'))
+    // const computo = fork(path.resolve(process.cwd(),'../../../utils/random.js'))
+    const computo = path.resolve(process.cwd(),'../../../utils/random.js')
     const cantidad = req.params.cantidad || 100000000
     computo.on('message', resultado => {
         if (resultado === 'preparado') {
